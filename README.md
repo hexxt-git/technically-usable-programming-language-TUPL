6ba0d833758134f5993056a4c286ff64a494d3c8
9EED <88392191+9EED@users.noreply.github.com>
2024-03-31 09:18:25 +0100

    day 2

--- README.md
+++ README.md
@@ -8,27 +8,40 @@ global is a special register it is used by most instructions
 ## instruction set
 lable:             starts a new label
 
-mov     reg1 reg2  moves the content of reg1 into reg2
+mov     reg1 reg2  moves the content of reg1 **to** reg2
 set     reg  val   sets a registry to a specified value
 
 jmp     label      jumps to a label
-//jmb   val        jumps by val lines
-//jmt   val        jumps to a line
+jmb     val        jumps by val lines
+jmt     val        jumps to a line
 
 if      val        only preformes the next val lines if global is truthy
 
 add     reg        adds a registry to global (parses to int)
-//sub   reg        subtracts a regisrtry from global
+sub     reg        subtracts a regisrtry from global
 mult    reg        multiplies global by a registry
-//div   reg        divides global by a registry
-//mod   reg        remainder of global by a registry
+div     reg        divides global by a registry
+mod     reg        remainder of global by a registry
+inc     reg        increments a registries value by 1
+
+cat     reg        concatinates a registry with global as strings
 
 et      reg        sets global to sets global to true if global is equal to reg
 gt      reg        sets global to true if global is greater than reg
-//lt    reg        sets global to true if global is lesser  than reg
+lt      reg        sets global to true if global is lesser  than reg
 
-log                logs out the global registrys value to the command line
-read               reads input from the command line
+and     reg        sets global to global and reg
+or      reg        sets global to global or reg
+not                sets global to not global
 
+prt     reg        logs out the global registrys value to the command line
+read    reg        reads user input from the command line
+log     val        logs out a custom message to the command line
+where              sets the global registers value to the current instruction's number
+
+sleep   val        sleeps for val ms
+sleer   reg        sleeps for a registries value in ms
 halt               ends the program
 pss                does nothing
+
+final time was an hour and a half for the base then ~ another hour polishing and making examples
--- index.js
+++ index.js
@@ -4,18 +4,13 @@ import fsp from 'fs/promises';
 if(process.argv.length < 3) console.error('please provide a .tupl file as argument')
 const file_name = process.argv[2]
 
-let program_text;
-
-program_text = await fsp.readFile(file_name, 'utf8');
-
+let program_text = await fsp.readFile(file_name, 'utf8');
 
 let program_instructions = program_text.split('\n').
     map(line => line.replaceAll(/^\s*|\s*&|\r|\s*#.*/g, '').replaceAll(/\s+/g, ' ')).
     map(line => line == '' ? 'pss' : line)
 
-let labels = program_instructions.reduce((labels, line, i) =>
-    line.slice(-1) == ':' ? [...labels, [line, i]] : labels, []
-)
+let labels = program_instructions.reduce((labels, line, i) => line.slice(-1) == ':' ? [...labels, [line, i]] : labels, [])
 
 let registers = {global: 0}
 let instruction_pointer = 0;
@@ -28,23 +23,45 @@ while(instruction_pointer < program_instructions.length){
     if (instruction == 'set') registers[args[0]] = args.slice(1).join(' ')
     
     if (instruction == 'jmp') instruction_pointer =  labels.find(label => label[0] == args[0]+':')[1]
-    if (instruction == 'if' ) if(registers.global) instruction_pointer += args[0]
+    if (instruction == 'jmt') instruction_pointer =  args[0]/1
+    if (instruction == 'jmb') instruction_pointer += args[0]/1
+    
+    if (instruction == 'if') if(registers.global) instruction_pointer += args[0]
 
     if (instruction == 'add' ) registers.global = registers.global/1 + registers[args[0]]/1
+    if (instruction == 'sub' ) registers.global = registers.global/1 - registers[args[0]]/1
     if (instruction == 'mult') registers.global = registers.global/1 * registers[args[0]]/1
+    if (instruction == 'div' ) registers.global = registers.global/1 / registers[args[0]]/1
+    if (instruction == 'mod' ) registers.global = registers.global/1 % registers[args[0]]/1
+    if (instruction == 'inc' ) registers[args[0]] = registers[args[0]]/1 + 1
 
-    if (instruction == 'et') registers.global = registers.global == registers[args[0]]
-    if (instruction == 'gt') registers.global = registers.global  > registers[args[0]]
+    if (instruction == 'cat' ) registers.global = registers.global + '' + registers[args[0]]
 
-    if (instruction == 'log') console.log(registers.global)
+    if (instruction == 'et') registers.global = registers.global/1 == registers[args[0]]/1
+    if (instruction == 'gt') registers.global = registers.global/1 > registers[args[0]]/1
+    if (instruction == 'lt') registers.global = registers.global/1 < registers[args[0]]/1
+
+    if (instruction == 'and') registers.global =  registers.global && registers[args[0]]
+    if (instruction == 'or' ) registers.global =  registers.global || registers[args[0]]
+    if (instruction == 'not') registers.global = !registers.global 
+
+    if (instruction == 'log') process.stdout.write(args.join(' ').split('\\n').join('\n'))
+    if (instruction == 'prt') process.stdout.write(registers[args[0]]+'')
     if (instruction == 'read') {
         let buf = Buffer.alloc(256);
         let len = fs.readSync(0, buf, 0, 256);
         let input = buf.toString('utf8', 0, len - 1); // -1 to remove the newline character
-        registers.global = input;
-    }
+        registers[args[0]] = input;
+    }    
+    if (instruction == 'where') registers.global = instruction_pointer
     
-    if (instruction == 'hlt') instruction == program_instructions.length + 1
+    if (instruction == 'sleep') {
+        await new Promise(resolve => setTimeout(resolve, args[0]/1))
+    }
+    if (instruction == 'sleer') {
+        await new Promise(resolve => setTimeout(resolve, registers[args[0]]/1))
+    }
+    if (instruction == 'hlt') instruction_pointer = program_instructions.length + 1
 
     instruction_pointer++
 }
